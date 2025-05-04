25b6ed4d0dc23fc2fa99886b8dc1e44a1b4fad8b
marcogulino-mg <marcogulino98@gmail.com>
2025-05-04 16:23:05 +0200

    feat: - add two Constructors (default value and params value). - add two instances of the Studente Class.

--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -2,6 +2,8 @@ package org.lessons.java.snacks;
 
 public class Main {
     public static void main(String[] args) {
+        Studente EmptyStudente = new Studente();
+        Studente Student01 = new Studente("Marco", "Gulino", (byte) 26);
 
     }
 }
--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -1,11 +1,25 @@
 package org.lessons.java.snacks;
 
 public class Studente {
+
     // * Attributes
     public String name;
     public String surname;
     public byte age;
 
     // * Methods
+
+    // * Constructors
+    public Studente() {
+        this.name = "Missing name";
+        this.surname = "Missing surname";
+        this.age = 0;
+    }
+
+    public Studente(String name, String surname, byte age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
 
 }
