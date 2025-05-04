370295257d52334e7a561eefa339c420d90e6ff6
marcogulino-mg <marcogulino98@gmail.com>
2025-05-04 17:33:50 +0200

    feat: - add RegistroStudenti Class - add a Constructor in RegistroStudenti Class - add students Array as Attribute of RegistroStudenti Class

---
+++ src/org/lessons/java/snacks/RegistroStudenti.java
@@ -0,0 +1,16 @@
+package org.lessons.java.snacks;
+
+import java.lang.reflect.Array;
+
+public class RegistroStudenti {
+    // * Attributes
+    private String[] students = new String[40];
+
+    // * Methods
+
+    // * Constructors
+    public RegistroStudenti(String[] students) {
+        this.students = students;
+    }
+
+}
