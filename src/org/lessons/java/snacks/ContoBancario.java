cf5e46e7298e3b66e51888af1fb9c5fa6cd366bd
marcogulino-mg <marcogulino98@gmail.com>
2025-05-04 16:36:40 +0200

    feat: - add ContoBancario Class - add Attributes in ContoBancario Class

---
+++ src/org/lessons/java/snacks/ContoBancario.java
@@ -0,0 +1,11 @@
+package org.lessons.java.snacks;
+
+import java.math.BigDecimal;
+
+public class ContoBancario {
+
+    // * Attributes
+    private String accountNumber;
+    private BigDecimal balance;
+
+}
--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -2,6 +2,9 @@ package org.lessons.java.snacks;
 
 public class Main {
     public static void main(String[] args) {
+
+        // ! SNACK 1
+
         // * Class Instances
         Studente EmptyStudente = new Studente();
         Studente Student01 = new Studente("Marco", "Gulino", (byte) 26);
@@ -9,5 +12,8 @@ public class Main {
         // * Methods
         System.out.println(EmptyStudente.concatData());
         System.out.println(Student01.concatData());
+
+        // ! SNACK 2
+
     }
 }
